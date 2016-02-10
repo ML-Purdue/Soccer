a3e3aa1546351b9741e2a8aada242c8e9b8740b0
Michael A Roth <roth28@purdue.edu>
2016-02-10 04:54:25 -0500

    Fixed XML documentation related warnings.

--- FootballSimulation/SteeringStrategies.cs
+++ FootballSimulation/SteeringStrategies.cs
@@ -3,12 +3,13 @@
 namespace FootballSimulation
 {
     /// <summary>
-    /// Steering Behaviors based on http://www.red3d.com/cwr/steer/gdc99/
+    ///     Steering Behaviors based on http://www.red3d.com/cwr/steer/gdc99/
     /// </summary>
+    // TODO: Finish in the XML documentation.
     public static class SteeringStrategies
     {
         /// <summary>
-        /// Move to and stop at a specified position.
+        ///     Move to and stop at a specified position.
         /// </summary>
         /// <param name="position"></param>
         /// <param name="target"></param>
@@ -28,36 +29,54 @@ namespace FootballSimulation
         }
 
         /// <summary>
-        /// Move toward a specified position.
+        ///     Move toward a specified position.
         /// </summary>
-        /// <param name="position"></param>
+        /// <param name="player"></param>
         /// <param name="target"></param>
+        /// <param name="desiredSpeed"></param>
         /// <returns></returns>
         public static Vector2 Seek(PointMass player, Vector2 target, float desiredSpeed)
         {
             var desiredVelocity = Vector2.Normalize(player.Position - target).ClampMagnitude(desiredSpeed);
             return desiredVelocity - player.Velocity;
         }
-        public static Vector2 SeekNormalized(PointMass player, Vector2 target, float desiredSpeed) => Vector2.Normalize(Seek(player, target, desiredSpeed));
 
         /// <summary>
-        /// Move away from a specified position.
         /// </summary>
-        /// <param name="position"></param>
+        /// <param name="player"></param>
         /// <param name="target"></param>
+        /// <param name="desiredSpeed"></param>
+        /// <returns></returns>
+        public static Vector2 SeekNormalized(PointMass player, Vector2 target, float desiredSpeed)
+            => Vector2.Normalize(Seek(player, target, desiredSpeed));
+
+        /// <summary>
+        ///     Move away from a specified position.
+        /// </summary>
+        /// <param name="player"></param>
+        /// <param name="target"></param>
+        /// <param name="desiredSpeed"></param>
         /// <returns></returns>
         public static Vector2 Flee(PointMass player, Vector2 target, float desiredSpeed)
         {
             var desiredVelocity = Vector2.Normalize(target - player.Position).ClampMagnitude(desiredSpeed);
             return desiredVelocity - player.Velocity;
         }
-        public static Vector2 FleeNormalized(PointMass player, Vector2 target, float desiredSpeed) => Vector2.Normalize(Flee(player, target, desiredSpeed));
 
         /// <summary>
-        /// Move toward a target's future position.
-        /// This method assumes the player will move at their maximum speed.  It also ignores friction on the target.
         /// </summary>
-        /// <param name="position"></param>
+        /// <param name="player"></param>
+        /// <param name="target"></param>
+        /// <param name="desiredSpeed"></param>
+        /// <returns></returns>
+        public static Vector2 FleeNormalized(PointMass player, Vector2 target, float desiredSpeed)
+            => Vector2.Normalize(Flee(player, target, desiredSpeed));
+
+        /// <summary>
+        ///     Move toward a target's future position. This method assumes the player will move at their maximum speed.  It also
+        ///     ignores friction on the target.
+        /// </summary>
+        /// <param name="player"></param>
         /// <param name="target"></param>
         /// <returns></returns>
         public static Vector2 Pursue(PointMass player, PointMass target)
@@ -91,9 +110,9 @@ namespace FootballSimulation
         }
 
         /// <summary>
-        /// Move away from a target's future position.
+        ///     Move away from a target's future position.
         /// </summary>
-        /// <param name="position"></param>
+        /// <param name="player"></param>
         /// <param name="target"></param>
         /// <returns></returns>
         public static Vector2 Evade(PointMass player, PointMass target) => Vector2.Zero;
