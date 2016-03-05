b674729dded6645038742f7f38dfa343b652e38f
Matthew T Page <page38@purdue.edu>
2016-03-05 16:04:28 -0500

    Implement Pursuit and fix Arrival again.

--- FootballSimulation/SteeringStrategies.cs
+++ FootballSimulation/SteeringStrategies.cs
@@ -13,21 +13,29 @@ namespace FootballSimulation
         /// </summary>
         /// <param name="player"></param>
         /// <param name="target"></param>
+        /// <param name="maxSpeed"></param>
         /// <param name="slowingRadius"></param>
         /// <returns></returns>
         public static Vector2 Arrive(PointMass player, Vector2 target, float maxSpeed, float slowingRadius)
         {
             var targetOffset = target - player.Position;
             var distance = targetOffset.Length();
-            if (distance == 0)
-            {
-                return SlowDown(player);
-            }
-            var rampedSpeed = player.MaxSpeed * (distance / slowingRadius);
+            var rampedSpeed = maxSpeed * (distance / slowingRadius);
             var clippedSpeed = Math.Min(rampedSpeed, player.MaxSpeed);
-            var desiredVelocity = clippedSpeed / distance * targetOffset;
+            var direction = targetOffset / distance;
+            var desiredVelocity = clippedSpeed * direction;
 
             return desiredVelocity - player.Velocity;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="player"></param>
+        /// <returns></returns>
+        public static float GetSlowingRadius(PointMass player)
+        {
+            return player.MaxSpeed * player.MaxSpeed / (2 * player.MaxForce / player.Mass);
         }
 
         /// <summary>
@@ -73,55 +81,77 @@ namespace FootballSimulation
         public static Vector2 FleeNormalized(PointMass player, Vector2 target, float desiredSpeed)
             => Vector2.Normalize(Flee(player, target, desiredSpeed));
 
+        private static int IntervalComparison(float x, float lower, float upper) => x < lower ? (x > upper ? 1 : 0) : -1;
+
         /// <summary>
         ///     Move toward a target's future position. This method assumes the player will move at their maximum speed.  It also
-        ///     ignores friction on the target.
+        ///     ignores friction on the target. Ported from https://github.com/meshula/OpenSteer/blob/master/include/OpenSteer/SteerLibrary.h
         /// </summary>
         /// <param name="player"></param>
         /// <param name="target"></param>
+        /// <param name="maxPredictionTime"></param>
         /// <returns></returns>
-        public static Vector2 Pursue(PointMass player, PointMass target)
+        public static Vector2 Pursue(PointMass player, PointMass target, float maxPredictionTime)
         {
-            // Calculate the time till intersection.
-            // We want to move the perpendicular distance between the player and the target in the same amount of time to move the parallel distance to the estimated intersection point.
-            // Formulas:
-            //     t = perpDist / Vy
-            //     Vx * t = initialParallelDist + Ball.ParallelVelocity * t         (ignores friction)
-            //       => Vx * perpDist / Vy = initialParallelDist + Ball.ParallelVelocity * perpDist / Vy
-            //       => Vx * perpDist = initialParallelDist * Vy + Ball.ParallelVelocity * perpDist
-            //       => Vx = initialParallelDist * Vy / perpDist + Ball.ParallelVelocity
-            //     player.MaxSpeed ^2 = Vx^2 + Vy^2
-            //       => player.MaxSpeed ^2 = (initialParallelDist * Vy / perpDist + Ball.ParallelVelocity)^2 + Vy^2
-            //       => lolno (keep these equations for future reference)
+            var offset = target.Position - player.Position;
+            var distance = offset.Length();
+            var unitOffset = offset / distance;
 
-            //     We can solve for Vy/Vx together, this is equal to tan(theta)
-            //     The above boils down to:
-            //     (1/Ball.ParallelVelocity)^2  = (initialParallelDist/(perpDist*Ball.ParallelVelocity))^2 * t^2 - 2*initialParallelDist/(perpDist * Ball.ParallelVelocity^2) = 1/maxSpeed^2 + t^2/maxSpeed^2
-            //              NOTE t is not time, it is Vy/Vx
-            //        Just solve this for t
-            //          https://www.wolframalpha.com/input/?i=t%5E2(d%5E2%2F(y%5E2*b%5E2)+-+1%2Fm%5E2)+-+2*d*t%2F(y*b%5E2)+%2B+1%2Fb%5E2+-+1%2Fm%5E2+%3D+0+solve+for+t
+            var parallelness = Vector2.Dot(Vector2.Normalize(player.Velocity), Vector2.Normalize(target.Velocity));
+            if (float.IsNaN(parallelness)) parallelness = 0;
+            var forwardness = Vector2.Dot(Vector2.Normalize(player.Velocity), unitOffset);
+            if (float.IsNaN(forwardness)) forwardness = 0;
 
-            //      => t = (sqrt(b^2 y^2 (y^2 (m^2-b^2)+d^2 m^2))+d m^2 y)/(d^2 m^2-b^2 y^2) if d^2 m^2!=b^2 y^2
-            //      => t = (sqrt(b^2 y^2 (y^2 (m^2-b^2)+d^2 m^2))-d m^2 y)/(b^2 y^2-d^2 m^2) if d^2 m^2!=b^2 y^2
-            //          NOTE t is not time, it is Vy/Vx
-            // The final angle we want to travel towards is:
-            // tan-1(t)
+            var f = IntervalComparison(forwardness, -0.707f, 0.707f);
+            var p = IntervalComparison(parallelness, -0.707f, 0.707f);
+            var t = 0f;
 
-            double initial_Distance = Vector2.Distance(player.Position, target.Position);
-            Vector2 initial_Vector = target.Position - player.Position;
-            double cos_angle_Distance_xAxis = Vector2.Dot(initial_Vector, Vector2.UnitX)/initial_Distance;
-            double angle_Distance_xAxis = Math.Acos(cos_angle_Distance_xAxis);
-            double cos_angle_targetVector_xAxis = Vector2.Dot(target.Velocity, Vector2.UnitX) / target.Velocity.Length();
-            double angle_targetVector_xAxis = Math.Acos(cos_angle_targetVector_xAxis);
-            double angle = angle_Distance_xAxis - angle_targetVector_xAxis;
-            double initial_Parellel_Distance = initial_Distance * Math.Cos(angle);
-            double initial_Perpendicular_Distance = initial_Distance * Math.Sin(angle);
-            double t = (initial_Parellel_Distance * Math.Pow((player.MaxSpeed), 2) * initial_Perpendicular_Distance + Math.Sqrt(Math.Pow(target.Velocity.Length(), 2) * Math.Pow(initial_Perpendicular_Distance, 2) * (Math.Pow(initial_Perpendicular_Distance, 2) * ((Math.Pow(player.MaxSpeed, 2)) - Math.Pow(target.Velocity.Length(), 2)) + Math.Pow(initial_Parellel_Distance, 2) * Math.Pow(player.MaxSpeed, 2)) / ((Math.Pow(initial_Parellel_Distance, 2)) * Math.Pow(player.MaxSpeed, 2) - Math.Pow(target.Velocity.Length(), 2) * Math.Pow(initial_Perpendicular_Distance, 2))));
-            double pursue_Angle = Math.Atan(t);
-            double angle_xAxis = pursue_Angle + angle_targetVector_xAxis;
-            Vector2 pursue = new Vector2((float)Math.Cos(angle_xAxis), (float)Math.Sin(angle_xAxis)) * player.MaxSpeed;
-            return pursue - player.Velocity; // This '- player.Velocity' is technically incorrect as the above assumes the player is at rest initially.  If major issues arise, remove this.
-                                             // If things still don't work, redo the math.
+            switch (f)
+            {
+                case 1:
+                    switch (p)
+                    {
+                        case 1: t = 4; break;
+                        case 0: t = 1.8f; break;
+                        case -1: t = 0.85f; break;
+                    }
+                    break;
+
+                case 0:
+                    switch (p)
+                    {
+                        case 1: t = 1f; break;
+                        case 0: t = 0.8f; break;
+                        case -1: t = 4; break;
+                    }
+                    break;
+
+                case -1:
+                    switch (p)
+                    {
+                        case 1: t = 0.5f; break;
+                        case 0: t = 2f; break;
+                        case -1: f = 2; break;
+                    }
+                    break;
+            }
+
+            var directTravelTime = distance / player.Velocity.Length();
+            float etl;
+
+            if (float.IsInfinity(directTravelTime))
+            {
+                etl = maxPredictionTime;
+            }
+            else
+            {
+                var et = directTravelTime * t;
+                etl = et > maxPredictionTime ? maxPredictionTime : et;
+            }
+
+            var targetPosition = target.Position + target.Velocity * etl;
+
+            return Seek(player, targetPosition, player.MaxSpeed);
         }
 
         /// <summary>
@@ -129,26 +159,29 @@ namespace FootballSimulation
         /// </summary>
         /// <param name="player"></param>
         /// <param name="target"></param>
+        /// <param name="maxPredictionTime"></param>
         /// <returns></returns>
-        public static Vector2 PursueNormalized(PointMass player, PointMass target)
-            => Vector2.Normalize(Pursue(player, target));
+        public static Vector2 PursueNormalized(PointMass player, PointMass target, float maxPredictionTime)
+            => Vector2.Normalize(Pursue(player, target, maxPredictionTime));
 
         /// <summary>
         ///     Move away from a target's future position.
         /// </summary>
         /// <param name="player"></param>
         /// <param name="target"></param>
+        /// <param name="maxPredictionTime"></param>
         /// <returns></returns>
-        public static Vector2 Evade(PointMass player, PointMass target)
-            => -1 * Pursue(player, target);
+        public static Vector2 Evade(PointMass player, PointMass target, float maxPredictionTime)
+            => -1 * Pursue(player, target, maxPredictionTime);
 
         /// <summary>
         /// 
         /// </summary>
         /// <param name="player"></param>
         /// <param name="target"></param>
+        /// <param name="maxPredictionTime"></param>
         /// <returns></returns>
-        public static Vector2 EvadeNormalized(PointMass player, PointMass target)
-            => Vector2.Normalize(Evade(player, target));
+        public static Vector2 EvadeNormalized(PointMass player, PointMass target, float maxPredictionTime)
+            => Vector2.Normalize(Evade(player, target, maxPredictionTime));
     }
 }
